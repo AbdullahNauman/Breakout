3b1f0fd682bc868ab04a2a8c67beb04a4ca75bfd
Christopher Moore <51140505+chmoore889@users.noreply.github.com>
2019-05-30 23:06:03 -0400

    Add files via upload

--- src/ArrowKeyListener.java
+++ src/ArrowKeyListener.java
@@ -4,61 +4,58 @@ import java.awt.event.KeyListener;
 public class ArrowKeyListener implements KeyListener
 {
 
-    public void keyTyped(KeyEvent e)
+  public void keyTyped(KeyEvent e)
+  {
+    
+  }
+
+  public void keyPressed(KeyEvent e)
+  {
+
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
     {
-
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT ) {
-            System.out.println("Right typed.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
-            System.out.println("Left typed.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_UP ) {
-            System.out.println("Up typed.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_DOWN ) {
-            System.out.println("Down typed.");
-        }
-        else{
-            System.out.println("Key typed: " + e.getKeyChar());
-        }
+      System.out.println("Right pressed.");
     }
-
-    public void keyPressed(KeyEvent e) {
-
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT ) {
-            System.out.println("Right pressed.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
-            System.out.println("Left pressed.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_UP ) {
-            System.out.println("Up pressed.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_DOWN ) {
-            System.out.println("Down pressed.");
-        }
-        else{
-            System.out.println("Key pressed: " + e.getKeyChar());
-        }
+    else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    {
+      System.out.println("Left pressed.");
     }
-
-    public void keyReleased(KeyEvent e) {
-
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT ) {
-            System.out.println("Right released.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
-            System.out.println("Left released.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_UP ) {
-            System.out.println("Up released.");
-        } 
-        else if (e.getKeyCode() == KeyEvent.VK_DOWN ) {
-            System.out.println("Down released.");
-        }
-        else{
-            System.out.println("Key released: " + e.getKeyChar());
-        }
+    else if (e.getKeyCode() == KeyEvent.VK_UP)
+    {
+      System.out.println("Up pressed.");
     }
+    else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+    {
+      System.out.println("Down pressed.");
+    }
+    else
+    {
+      System.out.println("Key pressed: " + e.getKeyChar());
+    }
+  }
+
+  public void keyReleased(KeyEvent e)
+  {
+
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+    {
+      System.out.println("Right released.");
+    }
+    else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+    {
+      System.out.println("Left released.");
+    }
+    else if (e.getKeyCode() == KeyEvent.VK_UP)
+    {
+      System.out.println("Up released.");
+    }
+    else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+    {
+      System.out.println("Down released.");
+    }
+    else
+    {
+      System.out.println("Key released: " + e.getKeyChar());
+    }
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,38 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Main
+public class Main extends JFrame
 {
+  public Main()
+  {
+    setTitle("Breakout");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public static void main(String[] args) 
-    {
+    // Getting screen size to start JFrame in middle of display and set
+    // appropriate size(looked up)
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int height = screenSize.height;
+    setSize(height * 9 / 16, (int) (height * 0.8));
 
-        JFrame frame = new JFrame("Breakout");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // setExtendedState(JFrame.MAXIMIZED_BOTH);
+    // setUndecorated(true);
 
-        JPanel panel = new JPanel();
+    // Centering JFrame
+    setLocationRelativeTo(null);
+    setResizable(false);
+  }
 
-        panel.addKeyListener(new ArrowKeyListener());
+  public static void main(String[] args)
+  {
 
-        frame.add(panel);
+    Main frame = new Main();
 
-        frame.setSize(500, 500);
-        frame.setVisible(true);
+    frame.add(new ImageFade("Were_Now_A_Game_Studio.png",0.15,2200));
+    //frame.add(new ImageFade("breakout.png",0.213,1800));
 
-        panel.requestFocusInWindow();
-    }
+    frame.setVisible(true);
+
+  }
 }
