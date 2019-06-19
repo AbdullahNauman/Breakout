385aadd89e94947aebad433ba7e0d263155bfa10
Christopher Moore <51140505+chmoore889@users.noreply.github.com>
2019-06-18 23:27:53 -0400

    Add files via upload

--- src/Paddle.java
+++ src/Paddle.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Paddle extends MovingObject implements KeyListener {
-	private static final int height = 10, origWidth = 40;
+	public static final int height = 10, origWidth = 40;
 	private static int width = origWidth;
 	// Paddle dimensions
 
